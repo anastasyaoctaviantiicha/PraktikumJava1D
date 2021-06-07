db7cc2fd92be995798f884cfdd00ab7a98dd6a54
anastasyaokta <andreiya6@gmail.com>
2021-06-07 22:33:16 +0800

    Input dengan Scanner

--- DataTypes.java
+++ DataTypes.java
@@ -1,7 +1,10 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DataTypes{
  public static void main(String[] args){
+
+ Scanner scanner = new Scanner(System.in);
 
  String namaDepan = "Anastasya";
  String namaBelakang = "Octavianti";
@@ -11,13 +14,27 @@ public class DataTypes{
  char nilaiAbjad ='A';
  boolean lulus = true;
  
+ System.out.print("Input Nama Depan : ");
+ namaDepan = scanner.nextLine();
+ System.out.print("Input Usia : ");
+ usia = scanner.nextInt();
+ System.out.print("Input IPK: ");
+ ipk = scanner.nextDouble();
+ System.out.print("");
+ System.out.print("Input Nilai Abjad : ");
+ nilaiAbjad = scanner.next().charAt(0);
+ System.out.print("Lulus? : ");
+ lulus = scanner.nextBoolean();
+
+ System.out.println("==========OUTPUT==========");
  System.out.println("Nama depan : " + namaDepan);
  System.out.println("Nama belakang : " + namaBelakang);
  System.out.println("Usia : " + usia);
- System.out.println("Target Kuliah : " + targetTahunKuliah + "tahun");
+ System.out.println("Target Kuliah : " +targetTahunKuliah + " tahun");
  System.out.println("IPK : " + ipk);
  System.out.println("Nilai PBO : " + nilaiAbjad);
  System.err.println("Lulus : " + lulus);
+ 
 
  JOptionPane.showMessageDialog(null,"Haii..." + namaDepan +" " + namaBelakang);
  }
