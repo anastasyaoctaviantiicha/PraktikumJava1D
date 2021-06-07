89fbe42031321f96312e3700f32f7d81ec8a833b
anastasyaokta <andreiya6@gmail.com>
2021-06-07 16:57:24 +0800

    deklarasi variabel dan tipe data

---
+++ DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes{
+ public static void main(String[] args){
+
+ String namaDepan = "Anastasya";
+ String namaBelakang = "Octavianti";
+ int usia = 20;
+ int targetTahunKuliah = 4;
+ double ipk = 3.54;
+ char nilaiAbjad ='A';
+ boolean cantik = true;
+ }
+}
