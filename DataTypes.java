86b3e625fb8820b54e6c8466166b18171724d9a0
anastasyaokta <andreiya6@gmail.com>
2021-06-07 17:27:20 +0800

    Output dengan sout

--- DataTypes.java
+++ DataTypes.java
@@ -7,6 +7,14 @@ public class DataTypes{
  int targetTahunKuliah = 4;
  double ipk = 3.54;
  char nilaiAbjad ='A';
- boolean cantik = true;
+ boolean lulus = true;
+ 
+ System.out.println("Nama depan : " + namaDepan);
+ System.out.println("Nama belakang : " + namaBelakang);
+ System.out.println("Usia : " + usia);
+ System.out.println("Target Kuliah : " + targetTahunKuliah);
+ System.out.println("IPK : " + ipk);
+ System.out.println("Nilai PBO : " + nilaiAbjad);
+ System.err.println("Lulus : " + lulus);
  }
 }
