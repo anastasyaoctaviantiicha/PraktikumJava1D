630cfcd9e29c66059b0f98e09e7b60207e5fd4bf
anastasyaokta <andreiya6@gmail.com>
2021-06-08 00:36:28 +0800

    Project IDEA

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+
+            String namaDepan = "Anastasya";
+            String namaBelakang = "Octavianti";
+            int usia = 20;
+            int targetTahunKuliah = 4;
+            double ipk = 3.54;
+            char nilaiAbjad ='A';
+            boolean lulus = true;
+
+            System.out.println("namaDepan + namaBelakang : ");
+    }
+}
