ca223c8147689417eb07ddb8434279f654ce914e
anastasyaokta <andreiya6@gmail.com>
2021-06-12 06:54:06 +0800

    Project IDEA

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,6 @@ public class Main {
             boolean lulus = true;
 
             System.out.println(namaDepan + namaBelakang);
-            char[] uniskaChar = {'U','N','I','S','K','A'};
-            String uniskaString = new String(uniskaChar);
-            System.out.println(uniskaString);
-            String namaLengkap = namaDepan + " " + namaBelakang;
-            System.out.println(namaLengkap.length());
-            System.out.println(namaLengkap.indexOf("NIS"));
-            System.out.println(namaLengkap.substring(5));
-            System.out.println(namaLengkap.substring(5,12));
-            System.out.println(namaLengkap.replace( "Ana","Vian"));
-            System.out.println(namaLengkap.toUpperCase());
-            System.out.println(namaLengkap.toLowerCase());
-            System.out.println(namaLengkap.charAt(6));
-
-            String[] namaArray = namaLengkap.split(" ");
-
-            for (String nama : namaArray){
-                  System.out.println(nama);
-            }
-
 
     }
 }
